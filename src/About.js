28c4bdd831ca47460adb3c2109483d3b19373b31
Joaquin1999 <joaquinjoe30799@gmail.com>
2023-08-17 12:10:37 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+      <h1>About</h1>
+      <p style={{marginTop:"1rem"}}>
+        Hi,This is Joaquin . the theme is to connect people to  entertainment and to spread joy
+      </p>
+      </main>
+  )
+}
+
+export default About
---
+++ src/App.js
@@ -0,0 +1,51 @@
+import {Routes,Route,Link,useNavigate}from "react-router-dom";
+import Header from "./Header"
+import About from "./About"
+import Nav from "./Nav"
+import NewPost from "./NewPost"
+import PostPage from "./PostPage"
+import Missing from "./Missing"
+import Footer from "./Footer"
+import Home from "./Home"
+import EditPost from "./EditPost"
+import {DataProvider} from "./Context/DataContext" ;
+
+function App() {
+  
+
+  
+  
+  return (
+    <div className="App">
+     <DataProvider>
+     <Header title="Joaquin's Entertainment" />
+     
+     <Nav/>
+
+     <Routes>
+  
+     <Route path="/" element={
+     <Home />} />
+
+     <Route path="post" >
+     <Route index element={<NewPost/>} />
+     <Route path=":id" element ={<PostPage />}/>
+      </Route>
+
+      <Route path="/edit/:id" element={<EditPost/>}/>
+     
+     
+     <Route path="about" element={<About/>}/>
+    <Route path="*" element={<Missing/>}/>
+     
+     
+     </Routes>
+
+     <Footer/>
+
+     </DataProvider>
+    </div>
+  );
+}
+
+export default App;
